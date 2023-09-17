685ddd11ad163e226920aaabb4a9c718bf2a6a4f
Diego Gonzalez <diegonzalezdevgmail.com>
2023-09-17 11:53:08 -0300

    class diagram funcionando, agregando metodos a clase administrador

--- Proyecto-Laboratorio2-v2/Proyecto-Laboratorio2-v2/Administrador.cpp
+++ Proyecto-Laboratorio2-v2/Proyecto-Laboratorio2-v2/Administrador.cpp
@@ -56,8 +56,96 @@ void Administrador::verSalasCargadas(const Sala* vecSalas) {
 		std::cout << std::endl;
 	}
 }
+
+Pelicula Administrador::seleccionarPelicula(Pelicula* vecPeliculas){
+
+	int indicePeliculaElegida;
+	Pelicula aux;
+	// Pide al usuario que elija una película del vector
+	std::cout << "Elija una película de la lista:" << std::endl;
+	for (int i = 0; i < 5; i++) {
+		std::cout << i + 1 << ". ";
+		vecPeliculas[i].mostrarDetalles(); // Muestra detalles de la película
+	}
+	std::cout << std::endl;
+	std::cout << "Opcion: ";
+	std::cin >> indicePeliculaElegida;
+
+	// Asegúrate de que el índice elegido esté dentro de los límites
+	if (indicePeliculaElegida >= 1 && indicePeliculaElegida <= 5) {
+		// Asigna la película elegida al objeto f1
+		aux = vecPeliculas[indicePeliculaElegida - 1];
+	}
+	else {
+		std::cout << "Opción no válida" << std::endl;
+	}
+	return aux;
+}
+
+Sala Administrador::seleccionarSala(Sala* vecSalas) {
+	int indiceSalaElegida;
+	Sala aux;
+	// Pide al usuario que elija una película del vector
+	std::cout << "Elija una sala de la lista:" << std::endl;
+	for (int i = 0; i < 5; i++) {
+		std::cout << i + 1 << ". ";
+		vecSalas[i].mostrarDetalles(); 
+	}
+	std::cout << std::endl;
+	std::cout << "Opcion: ";
+	std::cin >> indiceSalaElegida;
+
+	// valida de que el índice elegido esté dentro de los límites
+	if (indiceSalaElegida >= 1 && indiceSalaElegida <= 5) {
+		// Asigna la película elegida al objeto f1
+		aux = vecSalas[indiceSalaElegida - 1];
+	}
+	else {
+		std::cout << "Opción no válida" << std::endl;
+	}
+	return aux;
+}
+
+void Administrador::cargarFunciones(Funcion* vecFunciones,Pelicula* vecPeliculas, Sala* vecSalas) {
+	for (int i = 0; i < 2; i++) {
+		int idFuncion = i + 1;
+		Pelicula pelicula;
+		Sala sala;
+		FechaHorario fechaHoraFuncion;
+		int asientosDisponibles, dia , mes, anio, hora, minuto;
+		int asientosReservados;
+		Funcion funcion;
+		pelicula = seleccionarPelicula(vecPeliculas);
+		sala = seleccionarSala(vecSalas);
+		std::cout << "Ingrese el día de la función: " << std::endl;
+		std::cin >> dia;
+		std::cout << "Ingrese el mes de la función: " << std::endl;
+		std::cin >> mes;
+		std::cout << "Ingrese el año de la función: " << std::endl;
+		std::cin >> anio;
+		std::cout << "Ingrese la hora de la función: " << std::endl;
+		std::cin >> hora;
+		std::cout << "Ingrese los minutos de la función: " << std::endl;
+		std::cin >> minuto;
+		fechaHoraFuncion = FechaHorario(dia, mes, anio, minuto, hora);
+		std::cout << "Ingrese la cantidad de asientos disponibles: " << std::endl;
+		std::cin >> asientosDisponibles;
+		std::cout << "Ingrese la cantidad de asientos reservados: " << std::endl;
+		std::cin >> asientosReservados;
+		funcion = Funcion(pelicula, sala, fechaHoraFuncion, asientosDisponibles, asientosReservados);
+		vecFunciones[i] = funcion;
+	}
+}
+
+void Administrador::verFuncionesCargadas( Funcion* vecFunciones) {
+	for (int i = 0; i < 2; i++) {
+		vecFunciones[i].mostrarDetalles();
+		std::cout << std::endl;
+	}
+}
+
 Administrador::Administrador() : Persona() {
-	
+
 }
 
 Administrador::Administrador(int legajo, std::string cargo, std::string nombre, std::string apellido, std::string usuario, std::string contrasenia) : Persona(legajo, cargo, nombre, apellido, usuario, contrasenia) {
--- Proyecto-Laboratorio2-v2/Proyecto-Laboratorio2-v2/Sistema.cpp
+++ Proyecto-Laboratorio2-v2/Proyecto-Laboratorio2-v2/Sistema.cpp
@@ -7,11 +7,11 @@ using namespace std;
 
 Sistema::Sistema()
 {
-    Pelicula vecPeliculas[5];
-    Sala vecSalas[5];
-    Funcion vecFunciones[25];
-    _admin1 = Administrador(01, "DT", "Carlos", "Tevez", "Admin", "qwerty");
-    _vendedor1 = Vendedor(02, "Vendedor", "Lionel", "Messi", "Vendedor", "1234");
+	Pelicula vecPeliculas[5];
+	Sala vecSalas[5];
+	Funcion vecFunciones[25];
+	_admin1 = Administrador(01, "DT", "Carlos", "Tevez", "Admin", "qwerty");
+	_vendedor1 = Vendedor(02, "Vendedor", "Lionel", "Messi", "Vendedor", "1234");
 }
 
 Administrador Sistema::getAdmin() const {
@@ -21,77 +21,37 @@ Administrador Sistema::getAdmin() const {
 Vendedor Sistema::getVendedor() const {
 	return _vendedor1;
 }
+
 //funcion login
 
 void Sistema::login(Administrador admin1, Vendedor vendedor1) {
 
-    string usuario;
-    string contrasenia;
-    int intentos = 0;
-    bool login = false;
-    
-    do {
+	string usuario;
+	string contrasenia;
+	int intentos = 0;
+	bool login = false;
+
+	do {
 		system("cls");
 		cout << "Ingrese su usuario: ";
 		cin >> usuario;
 		cout << "Ingrese su contrasenia: ";
 		cin >> contrasenia;
-        if (usuario == admin1.getUsuario() && contrasenia == admin1.getContrasenia()) {
+		if (usuario == admin1.getUsuario() && contrasenia == admin1.getContrasenia()) {
 			cout << "Bienvenido " << admin1.getNombre() << " " << admin1.getApellido() << endl;
 			login = true;
 			system("pause");
 			system("cls");
 			mostrarMenuAdmin();
 		}
-        else if (usuario == vendedor1.getUsuario() && contrasenia == vendedor1.getContrasenia()) {
-			cout << "Bienvenido " << vendedor1.getNombre() << " " << vendedor1.getApellido() << endl;
-			login = true;
-			system("pause");
-			system("cls");
-			mostrarMenuVendedor();
-		}
-        else {
-			cout << "Usuario o contrasenia incorrectos" << endl;
-			intentos++;
-			system("pause");
-			system("cls");
-		}
-	} while (intentos < 3 && login == false);
-}
-
-Vendedor Sistema::getVendedor() const {
-	return _vendedor1;
-}
-//funcion login
-
-void Sistema::login(Administrador admin1, Vendedor vendedor1) {
-
-    string usuario;
-    string contrasenia;
-    int intentos = 0;
-    bool login = false;
-    
-    do {
-		system("cls");
-		cout << "Ingrese su usuario: ";
-		cin >> usuario;
-		cout << "Ingrese su contrasenia: ";
-		cin >> contrasenia;
-        if (usuario == admin1.getUsuario() && contrasenia == admin1.getContrasenia()) {
-			cout << "Bienvenido " << admin1.getNombre() << " " << admin1.getApellido() << endl;
-			login = true;
-			system("pause");
-			system("cls");
-			mostrarMenuAdmin();
-		}
-        else if (usuario == vendedor1.getUsuario() && contrasenia == vendedor1.getContrasenia()) {
+		else if (usuario == vendedor1.getUsuario() && contrasenia == vendedor1.getContrasenia()) {
 			cout << "Bienvenido " << vendedor1.getNombre() << " " << vendedor1.getApellido() << endl;
 			login = true;
 			system("pause");
 			system("cls");
 			mostrarMenuAdmin();
 		}
-        else {
+		else {
 			cout << "Usuario o contrasenia incorrectos" << endl;
 			intentos++;
 			system("pause");
@@ -103,116 +63,102 @@ void Sistema::login(Administrador admin1, Vendedor vendedor1) {
 // Funcion para mostrar las opciones del menu
 void showItem(const char* text, int posx, int posy, bool selected) {
 
-    if (selected) {
-        rlutil::setBackgroundColor(rlutil::COLOR::BLUE);
-        rlutil::locate(posx - 3, posy); // posiciona el cursor en la fila y columna que le pasamos por parametro (en este caso, -2 porque colocamos una flechita en la opcion seleccionada)
-        std::cout << (char) 175 << "  " << text << "  " << (char) 174 << std::endl; // imprime una flechita a cada lado con el codigo ASCII, y el texto que le pasamos por parametro
-    }
-    else {
-        rlutil::setBackgroundColor(rlutil::COLOR::BLACK);
-        rlutil::locate(posx-3, posy);
-        std::cout << "   " << text << "   " <<  std::endl; // si no esta seleccionado, imprime el texto sin las flechitas
-    }
-    rlutil::setBackgroundColor(rlutil::COLOR::BLACK); // cuando llega a la ultima opcion, cambia el color del cursor al color normal
+	if (selected) {
+		rlutil::setBackgroundColor(rlutil::COLOR::BLUE);
+		rlutil::locate(posx - 3, posy); // posiciona el cursor en la fila y columna que le pasamos por parametro (en este caso, -2 porque colocamos una flechita en la opcion seleccionada)
+		std::cout << (char)175 << "  " << text << "  " << (char)174 << std::endl; // imprime una flechita a cada lado con el codigo ASCII, y el texto que le pasamos por parametro
+	}
+	else {
+		rlutil::setBackgroundColor(rlutil::COLOR::BLACK);
+		rlutil::locate(posx - 3, posy);
+		std::cout << "   " << text << "   " << std::endl; // si no esta seleccionado, imprime el texto sin las flechitas
+	}
+	rlutil::setBackgroundColor(rlutil::COLOR::BLACK); // cuando llega a la ultima opcion, cambia el color del cursor al color normal
 }
 
 
 
 void Sistema::mostrarMenuAdmin() {
-    
-    int op = 1, y=0;
-    Funcion f1;
 
-    do {
-        rlutil::setConsoleTitle("MENU ADMIN CINE"); // establece el titulo de la consola
-        rlutil::hidecursor(); // oculta el cursor
-        //rlutil::cls(); // limpia la pantalla
+	int op = 1, y = 0;
+	Funcion f1;
 
-        showItem(" Cargar peliculas ", 50, 10, y == 0); //si  y  es igual a 0, la opcion 1 esta seleccionada, coloca alli el cursor y cambia el color de fondo con la funcion showItem
-        showItem(" Ver peliculas cargadas ", 50, 11, y == 1);
-        showItem(" Cargar salas ", 50, 12, y == 2);
-        showItem(" Ver salas cargadas ", 50, 13, y == 3);
-        showItem(" OPCION 5 ", 50, 14, y == 4);
-        showItem("  SALIR   ", 50, 15, y == 5);
+	do {
+		rlutil::setConsoleTitle("MENU ADMIN CINE"); // establece el titulo de la consola
+		rlutil::hidecursor(); // oculta el cursor
+		//rlutil::cls(); // limpia la pantalla
 
-        int key = rlutil::getkey(); // Lee una pulsación de tecla y devuelve un código ASCII de tecla.
+		showItem(" Cargar peliculas ", 50, 10, y == 0); //si  y  es igual a 0, la opcion 1 esta seleccionada, coloca alli el cursor y cambia el color de fondo con la funcion showItem
+		showItem(" Ver peliculas cargadas ", 50, 11, y == 1);
+		showItem(" Cargar salas ", 50, 12, y == 2);
+		showItem(" Ver salas cargadas ", 50, 13, y == 3);
+		showItem(" Cargar Funciones ", 50, 14, y == 4);
+		showItem(" Ver salas cargadas ", 50, 15, y == 5);
+		showItem("  SALIR   ", 50, 16, y == 6);
+
+		int key = rlutil::getkey(); // Lee una pulsación de tecla y devuelve un código ASCII de tecla.
 
 
 
-        switch (key) // evalua el codigo de tecla
-        {
-        case 14: // flecha ARRIBA
-            rlutil::locate(28, 10 + y);
-            std::cout << " " << std::endl; // imprime un espacio para borrar la flecha cuando cambie de posicion
-            y--;
-            if(y<0) y=0; // validacion para que no se salga de las opciones
-            break;
-        case 15: // flecha ABAJO
-            rlutil::locate(28, 10 + y);
-            std::cout << " " << std::endl;
-            y++;
-            if(y>5) y=5;
-            break;
-        case 1: // ENTER
-            switch (y)
-            {      
-            case 0:{
-                system("cls");
-                _admin1.cargarPeliculas(vecPeliculas);                          
-                system("cls");
-                break;
-            }
-            case 1:
-                system("cls");
-                _admin1.verPeliculasCargadas(vecPeliculas);
-                system("pause");
-                system("cls");
-                break;
-            case 2:
-                system("cls");
-                _admin1.cargarSalas(vecSalas);
-                system("cls");
-                break;
-                case 3:
-                    system("cls");
-                    _admin1.verSalasCargadas(vecSalas);
-                    system("pause");
-                    system("cls");
-                    break;
-                    case 4:
-                    system("cls");
-                    int indicePeliculaElegida;
+		switch (key) // evalua el codigo de tecla
+		{
+		case 14: // flecha ARRIBA
+			rlutil::locate(28, 10 + y);
+			std::cout << " " << std::endl; // imprime un espacio para borrar la flecha cuando cambie de posicion
+			y--;
+			if (y < 0) y = 0; // validacion para que no se salga de las opciones
+			break;
+		case 15: // flecha ABAJO
+			rlutil::locate(28, 10 + y);
+			std::cout << " " << std::endl;
+			y++;
+			if (y > 6) y = 6;
+			break;
+		case 1: // ENTER
+			switch (y)
+			{
+			case 0: {
+				system("cls");
+				_admin1.cargarPeliculas(vecPeliculas);
+				system("cls");
+				break;
+			}
+			case 1:
+				system("cls");
+				_admin1.verPeliculasCargadas(vecPeliculas);
+				system("pause");
+				system("cls");
+				break;
+			case 2:
+				system("cls");
+				_admin1.cargarSalas(vecSalas);
+				system("cls");
+				break;
+			case 3:
+				system("cls");
+				_admin1.verSalasCargadas(vecSalas);
+				system("pause");
+				system("cls");
+				break;
+			case 4:
+				system("cls");
+				_admin1.cargarFunciones(vecFunciones, vecPeliculas, vecSalas);
+				system("pause");
+				system("cls");
+				break;
 
-                    // Pide al usuario que elija una película del vector
-                    cout << "Elija una película de la lista:" << endl;
-                    for (int i = 0; i < 5; i++) {
-                        cout << i + 1 << ". ";
-                        vecPeliculas[i].mostrarDetalles(); // Muestra detalles de la película
-                    }
-                    cout << endl;
-                    cout << "Opcion: ";
-                    cin >> indicePeliculaElegida;
-
-                    // Asegúrate de que el índice elegido esté dentro de los límites
-                    if (indicePeliculaElegida >= 1 && indicePeliculaElegida <= 5) {
-                        // Asigna la película elegida al objeto f1
-                        f1.setPelicula(vecPeliculas[indicePeliculaElegida - 1]);
-                        f1.getPelicula().mostrarDetalles();
-                    }
-                    else {
-                        cout << "Opción no válida" << endl;
-                    }
-                    system("pause");
-                    system("cls");
-                    break;
-						
-
-            case 5: // Si el cursor esta en la opcion SALIR
-                   op = 0; // sale del programa
-                   break;
-            }
-        }
-    } while (op != 0);
+			case 5:
+				system("cls");
+				_admin1.verFuncionesCargadas(vecFunciones);
+				system("pause");
+				system("cls");
+				break;
+			case 6: // Si el cursor esta en la opcion SALIR
+				op = 0; // sale del programa
+				break;
+			}
+		}
+	} while (op != 0);
 
 
 
@@ -220,62 +166,62 @@ void Sistema::mostrarMenuAdmin() {
 
 void Sistema::mostrarMenuVendedor() {
 
-    int op = 1, y = 0;
-    Funcion f1;
+	int op = 1, y = 0;
+	Funcion f1;
 
-    do {
-        rlutil::setConsoleTitle("MENU VENDEDOR CINE"); // establece el titulo de la consola
-        rlutil::hidecursor(); // oculta el cursor
-        //rlutil::cls(); // limpia la pantalla
+	do {
+		rlutil::setConsoleTitle("MENU VENDEDOR CINE"); // establece el titulo de la consola
+		rlutil::hidecursor(); // oculta el cursor
+		//rlutil::cls(); // limpia la pantalla
 
-        showItem(" OPCION 1 ", 50, 10, y == 0); //si  y  es igual a 0, la opcion 1 esta seleccionada, coloca alli el cursor y cambia el color de fondo con la funcion showItem
-        showItem(" OPCION 2 ", 50, 11, y == 1);
-        showItem(" OPCION 3 ", 50, 12, y == 2);
-        showItem(" OPCION 4 ", 50, 13, y == 3);
-        showItem(" OPCION 5 ", 50, 14, y == 4);
-        showItem("  SALIR   ", 50, 15, y == 5);
+		showItem(" OPCION 1 ", 50, 10, y == 0); //si  y  es igual a 0, la opcion 1 esta seleccionada, coloca alli el cursor y cambia el color de fondo con la funcion showItem
+		showItem(" OPCION 2 ", 50, 11, y == 1);
+		showItem(" OPCION 3 ", 50, 12, y == 2);
+		showItem(" OPCION 4 ", 50, 13, y == 3);
+		showItem(" OPCION 5 ", 50, 14, y == 4);
+		showItem("  SALIR   ", 50, 15, y == 5);
 
-        int key = rlutil::getkey(); // Lee una pulsación de tecla y devuelve un código ASCII de tecla.
+		int key = rlutil::getkey(); // Lee una pulsación de tecla y devuelve un código ASCII de tecla.
 
 
 
-        switch (key) // evalua el codigo de tecla
-        {
-        case 14: // flecha ARRIBA
-            rlutil::locate(28, 10 + y);
-            std::cout << " " << std::endl; // imprime un espacio para borrar la flecha cuando cambie de posicion
-            y--;
-            if (y < 0) y = 0; // validacion para que no se salga de las opciones
-            break;
-        case 15: // flecha ABAJO
-            rlutil::locate(28, 10 + y);
-            std::cout << " " << std::endl;
-            y++;
-            if (y > 5) y = 5;
-            break;
-        case 1: // ENTER
-            switch (y)
-            {
-            case 0: {
-                
-                break;
-            }
-            case 1:
-              
-                break;
-            case 2:
-                
-                break;
-            case 3:
-                
-                break;
-            case 4:
-              
-                break;
-            case 5: // Si el cursor esta en la opcion SALIR
-                op = 0; // sale del programa
-                break;
-            }
-        }
-    } while (op != 0);
+		switch (key) // evalua el codigo de tecla
+		{
+		case 14: // flecha ARRIBA
+			rlutil::locate(28, 10 + y);
+			std::cout << " " << std::endl; // imprime un espacio para borrar la flecha cuando cambie de posicion
+			y--;
+			if (y < 0) y = 0; // validacion para que no se salga de las opciones
+			break;
+		case 15: // flecha ABAJO
+			rlutil::locate(28, 10 + y);
+			std::cout << " " << std::endl;
+			y++;
+			if (y > 5) y = 5;
+			break;
+		case 1: // ENTER
+			switch (y)
+			{
+			case 0: {
+
+				break;
+			}
+			case 1:
+
+				break;
+			case 2:
+
+				break;
+			case 3:
+
+				break;
+			case 4:
+
+				break;
+			case 5: // Si el cursor esta en la opcion SALIR
+				op = 0; // sale del programa
+				break;
+			}
+		}
+	} while (op != 0);
 }
