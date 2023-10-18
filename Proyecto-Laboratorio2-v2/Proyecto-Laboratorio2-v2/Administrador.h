f75146c18b18b56a1235c0187cabe7cac77cad5c
Diego Gonzalez <diegonzalezdevgmail.com>
2023-10-17 22:17:13 -0300

    comienzo documentacion de funciones administrador

--- Proyecto-Laboratorio2-v2/Proyecto-Laboratorio2-v2/Administrador.h
+++ Proyecto-Laboratorio2-v2/Proyecto-Laboratorio2-v2/Administrador.h
@@ -18,20 +18,91 @@ class Administrador : public Persona
 private:
 
 public:
+	/**
+	* @brief Carga completa de los parametros de la clase Pelicula con id autoincremental y graba en registro.
+	* @param void
+	* @return void
+	* 	*/
 	void cargarPeliculas();
+	/**
+	* @brief Recorre el archivo de peliculas y muestra los registros que no tengan baja logica.
+	* @param void
+	* @return void
+	*/
 	void verPeliculasCargadas();
+	/**
+	* @brief Carga completa de los parametros de la clase Sala y graba en registro.
+	* @param void
+	* @return void
+	*/
 	void cargarSalas();
+	/**
+	* @brief Recorre el archivo de salas con id autoincremental y muestra los registros que no tengan baja logica.
+	* @param void
+	* @return void
+	*/
 	void verSalasCargadas();
+	/**
+	* @brief Recorre el archivo de peliculas, muestra los registros que no tengan baja logica, pide el ingreso de un ID y devuelve el registro que coincida con el ID ingresado.
+	* @param void
+	* @return Objeto Pelicula selecionada
+	*/
 	Pelicula seleccionarPelicula();
+	/**
+	* @brief Recibe un id de pelicula, con ese id busca la posicion en el archivo de peliculas y devuelve el registro que coincida con el id ingresado.
+	* @param int valorBuscado
+	* @return Objeto Pelicula buscado xID
+	*/
 	Pelicula buscarPeliculaxID(int valorBuscado);
+	/**
+	* @brief Recorre el archivo de peliculas, muestra los registros que no tengan baja logica, pide el ingreso de un ID, busca la posicion en el archivo de peliculas, da baja logica al registro y lo graba en el archivo con validacion y muestra por pantalla si pudo o no realizar la accion.
+	* @param void
+	* @return true o false segun si pudo o no realizar la accion
+	*/
 	bool darDeBajaPelicula();
+	/**
+	* @brief Recorre el archivo de salas, muestra los registros que no tengan baja logica, pide el ingreso de un ID y devuelve el registro que coincida con el ID ingresado.
+	* @param void
+	* @return Sala
+	*/
 	Sala seleccionarSala();
+	/**
+	* @brief Recibe un id de sala, con ese id busca la posicion en el archivo de salas y devuelve el registro que coincida con el id ingresado.
+	* @param int valorBuscado
+	* @return Objeto Sala buscado xID
+ 	*/
 	Sala buscarSalaxID(int valorBuscado);
+	/**
+	* @brief Recibe un id de sala, con ese id busca la posicion en el archivo de salas, solicita por pantalla el nuevo valor de la entrada a la sala, modifica el registro , lo graba en el archivo con validacion y muestra por pantalla si pudo o no realizar la accion.
+	* @param int nroSala
+	* @return true o false segun si pudo o no realizar la accion
+ 	*/
 	bool modificarSalaEnRegistro(int nroSala);
+	/**
+	* @brief Recorre el archivo de salas, muestra los registros que no tengan baja logica, pide el ingreso de un ID, busca la posicion en el archivo de salas, da baja logica al registro, lo graba en el archivo con validacion y muestra por pantalla si pudo o no realizar la accion.
+	* @param void
+	* @return true o false segun si pudo o no realizar la accion
+	*/
 	bool darDeBajaSala();
+	/**
+	* @brief Carga de un objeto funcion con id autoincremental, crea objeto Diagrama con mismo id de funcion, elige pelicula con seleccionarPelicula(), elige sala con seleccionarSala(), pide por pantalla el dia y hora de la funcion, graba en registro.
+	* @param void
+	* @return void
+	*/
 	void cargarFunciones();
+	/**
+	* @brief Recorre el archivo de funciones y muestra los registros que no tengan baja logica.
+	* @param void
+	* @return void
+	*/
 	void verFuncionesCargadas();
+	/**
+	* @brief Recibe un id de funcion, con ese id busca la posicion en el archivo de funciones, captura el registro que coincida con el id y abre un menu para modificar la funcion.
+	* @param int idFuncion
+	* @return true o false segun si pudo o no realizar la accion
+	*/
 	bool modificarFuncionEnRegistro(int idFuncion);
+	
 	bool verificarEstadoPeliculas(int idPelicula);
 	bool verificarEstadoSalas(int idSaLa);
 	void darDeBajaFuncionxSalaOxPelicula();
